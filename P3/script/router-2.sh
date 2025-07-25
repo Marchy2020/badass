d38010badf4e5585a9fb5ae90001ceb746ca0698
Antoine Clarman <antoinereims28@gmail.com>
2025-07-25 15:58:07 +0200

    chore: add p3

---
+++ P3/script/router-2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# peut etre le mettre dans un fichier interfaces
+/sbin/ip link add br0 type bridge
+/sbin/ip link set dev br0 up
+/sbin/ip link add vxlan10 type vxlan id 10 dstport 4789
+/sbin/ip link set dev vxlan10 up
+brctl addif br0 vxlan10
+brctl addif br0 eth1
+
+vtysh << EOF
+
+conf t
+
+interface eth0
+  ip address 10.1.1.2/30
+  ip ospf area 0
+interface lo
+  ip address 1.1.1.2/32
+  ip ospf area 0
+router ospf
+  router-id 1.1.1.2
+  network 0.0.0.0/0 area 0
+router bgp 1
+  neighbor 1.1.1.1 remote-as 1
+  neighbor 1.1.1.1 update-source lo
+
+  address-family l2vpn evpn
+    neighbor 1.1.1.1 activate
+    advertise-all-vni
+  exit-address-family
+do write
+EOF
